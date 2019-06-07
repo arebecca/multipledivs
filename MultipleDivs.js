6d367ba22b39452478130d4b37acd70728c5787d
grigorianel <48581720+grigorianel@users.noreply.github.com>
2019-06-07 15:33:26 +0400

    Update MultipleDivs.js

--- MultipleDivs.js
+++ MultipleDivs.js
@@ -1,6 +1,6 @@
 const increment = () => {
- increment.counter = typeof increment.counter == 'undefined' ? 0 : increment.counter + 1;
- return increment.counter;
+    increment.counter = typeof increment.counter == 'undefined' ? 0 : increment.counter + 1;
+    return increment.counter;
 };
 const letters = '0123456789ABCDEF';
 const getRandomColor = () => [0, 1, 2, 3, 4, 5].reduce(acc => acc + letters[Math.floor(Math.random() * 16)], '#');
@@ -9,8 +9,8 @@ const addItem = () => {
     const id = increment();
     const newDiv = document.createElement('div');
     newDiv.innerHTML = `<span id="buttons">
-        <button type="button" onclick="addItem()"> Add</button>
-        <button type="button" onclick="removeItem(${id})"> Remove</button>
+        <button type="button" onclick="addItem()">Add Item</button>
+        <button type="button" onclick="removeItem(${id})">Remove</button>
       </span>`;
     newDiv.setAttribute('id', `item_${id}`);
     newDiv.style.backgroundColor = getRandomColor();
